3400725f7b6eff1fbf669eb2c16d24ea983a35f8
rikuyu <rikuyu5884@gmail.com>
2024-01-29 10:50:41 +0900

    progress bar

--- feature/catalog/src/main/kotlin/com/example/feature/museum/circularprogressbar/CircularProgressBar.kt
+++ feature/catalog/src/main/kotlin/com/example/feature/museum/circularprogressbar/CircularProgressBar.kt
@@ -1,12 +1,17 @@
 package com.example.feature.museum.circularprogressbar
 
-import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateInt
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -16,101 +21,102 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithCache
-import androidx.compose.ui.geometry.toRect
-import androidx.compose.ui.graphics.BlendMode
-import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.graphics.drawscope.rotate
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.graphics.withSaveLayer
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.core.graphics.ColorUtils
 
 @Composable
-internal fun CircularProgressBar(
+fun CircularProgressBar(
     modifier: Modifier = Modifier,
+    currentValue: Int = 75,
+    maxValue: Int = 100,
+    progressBackgroundColor: Color = Color.Blue.copy(alpha = 0.2f),
+    progressIndicatorColor: Color = Color.Blue,
 ) {
-    var transitionState by remember { mutableStateOf(false) }
-    val transition = updateTransition(targetState = transitionState, label = "")
+    var startAnimation by remember { mutableStateOf(false) }
+    val transition = updateTransition(targetState = startAnimation, label = "")
 
-    val current = 0.75f
-    val colors = listOf(
-        Color(0xFF3B91C4),
-        Color(0xFFF84CAD),
-    )
-    val progressTextColor = Color(
-        ColorUtils.blendARGB(colors[0].toArgb(), colors[1].toArgb(), current),
-    )
-    val textColor by transition.animateColor(
-        transitionSpec = { tween(durationMillis = 2000) },
-        targetValueByState = { if (it) progressTextColor else colors[0] },
+    val barAnim by transition.animateFloat(
+        transitionSpec = { tween(durationMillis = 2500) },
         label = "",
-    )
+    ) { if (it) currentValue / maxValue.toFloat() else 0f }
+
     val progress by transition.animateInt(
-        transitionSpec = { tween(durationMillis = 2000) },
-        targetValueByState = { if (it) (100 * current).toInt() else 0 },
+        transitionSpec = { tween(durationMillis = 2500) },
         label = "",
-    )
-    val angle by transition.animateFloat(
-        transitionSpec = { tween(durationMillis = 2000) },
-        targetValueByState = { if (it) 360f * current else 0f },
-        label = "",
-    )
-    val progressText = buildAnnotatedString {
-        append("$progress")
-        withStyle(style = SpanStyle(fontSize = 40.sp)) {
-            append("%")
-        }
-    }
-    val density = LocalDensity.current
-
-    LaunchedEffect(Unit) { transitionState = true }
-
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = modifier
-            .size(400.dp)
-            .drawWithCache {
-                val paint = Paint()
-                val brush = Brush.sweepGradient(colors)
-                val strokeWidth = with(density) { 80.dp.toPx() }
-                onDrawWithContent {
-                    drawContent()
-                    drawIntoCanvas { canvas ->
-                        canvas.withSaveLayer(bounds = size.toRect(), paint) {
-                            drawCircle(
-                                color = Color.LightGray,
-                                radius = size.minDimension / 2 - strokeWidth / 2,
-                                style = Stroke(width = strokeWidth),
-                            )
-                            rotate(degrees = -90f) {
-                                drawArc(
-                                    brush = brush,
-                                    startAngle = 0f,
-                                    sweepAngle = angle,
-                                    useCenter = true,
-                                    blendMode = BlendMode.SrcAtop,
-                                )
-                            }
-                        }
-                    }
-                }
-            },
     ) {
-        Text(
-            text = progressText,
-            style = TextStyle(fontSize = 80.sp),
-            color = textColor,
+        if (it) currentValue else 0
+    }
+
+    LaunchedEffect(Unit) { startAnimation = true }
+
+    Box(modifier = modifier) {
+        val canvasSize = 200
+        val stroke = with(LocalDensity.current) {
+            Stroke(
+                width = 4.dp.toPx(),
+                cap = StrokeCap.Round,
+                join = StrokeJoin.Round,
+            )
+        }
+        Surface(
+            color = Color.Red,
+            shape = CircleShape,
+            modifier = Modifier
+                .size((canvasSize - stroke.width).dp)
+                .align(Alignment.Center),
+            content = {},
         )
+
+        Canvas(Modifier.size(canvasSize.dp)) {
+            val startAngle = 90f
+            val sweep = barAnim * 360f
+            val diameterOffset = stroke.width / 2
+            val arcRadius = size.width - 2 * diameterOffset
+
+            drawArc(
+                color = progressBackgroundColor,
+                startAngle = 360f,
+                sweepAngle = 360f,
+                useCenter = false,
+                topLeft = Offset(diameterOffset, diameterOffset),
+                size = Size(arcRadius.toDp().toPx(), arcRadius.toDp().toPx()),
+                style = stroke,
+            )
+
+            drawArc(
+                color = progressIndicatorColor,
+                startAngle = startAngle,
+                sweepAngle = sweep,
+                useCenter = false,
+                topLeft = Offset(diameterOffset, diameterOffset),
+                size = Size(arcRadius.toDp().toPx(), arcRadius.toDp().toPx()),
+                style = stroke,
+            )
+        }
+
+        Surface(
+            color = Color.Black,
+            shape = CircleShape,
+            modifier = Modifier
+                .widthIn(min = 90.dp)
+                .offset(y = 8.dp)
+                .align(Alignment.BottomCenter),
+        ) {
+            Text(
+                text = "${progress}%完了",
+                color = Color.White,
+                modifier = Modifier
+                    .padding(
+                        horizontal = 12.dp,
+                        vertical = 4.dp,
+                    ),
+            )
+        }
     }
 }
